3fbce9c4288cdee62bbee063d6367a89114a4af1
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-09-24 13:22:45 +0200

    [arquillian/arquillian-extension-liferay#30] Add sample portlet following liferay-maven-plugin development guide
    It has been copied from a portlet generated with Liferay IDE (see https://www.liferay.com/es/documentation/liferay-portal/6.2/development/-/ai/developing-plugins-using-maven-liferay-portal-6-2-dev-guide-02-en)

---
+++ arquillian-extension-liferay-example/src/main/java/org/arquillian/liferay/sample/activator/SampleBundleActivator.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package org.arquillian.liferay.sample.activator;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class SampleBundleActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Bootstrapping resources in the bundle...");
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Destroying resources in the bundle...");
+	}
+
+}
